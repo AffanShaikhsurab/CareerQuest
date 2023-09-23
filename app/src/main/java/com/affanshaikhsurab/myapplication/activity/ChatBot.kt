90b3f0a0a6958db745d6ccd6f862d487763ac5a9
AffanShaikhsurab <affanshaikhsurabofficial@gmail.com>
2023-09-23 21:51:18 +0530

    ..

---
+++ app/src/main/java/com/affanshaikhsurab/myapplication/activity/ChatBot.kt
@@ -0,0 +1,12 @@
+package com.affanshaikhsurab.myapplication.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.affanshaikhsurab.myapplication.R
+
+class ChatBot : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_bot)
+    }
+}
